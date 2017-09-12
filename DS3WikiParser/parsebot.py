b0c05c424690e2f22e5fe605c1ed13e71a20af16
novaJeb <novajebbro@gmail.com>
2017-09-12 15:26:44 -0500

    Fixes

--- DS3WikiParser/parsebot.py
+++ DS3WikiParser/parsebot.py
@@ -22,25 +22,27 @@ with open('parseme.txt', 'r') as source_file:
                 if right_carrots == 3:
                     found_trigger = True
             new_string = new_string[::-1][1:-10]
-##            print(stat_array)
+            print(stat_array)
             grand_array.append(stat_array)
             stat_array=[]
             stat_array.append(new_string)
-##            print(stat_array)
+            print(stat_array)
 ##            if count != 0: stat_array = []
         if line[:12]=='<td style="t':
             stat_array.append(line.strip()[45:-5])
         count += 1
     grand_array.append(stat_array)
-##    print(stat_array)
+    print(stat_array)
 ##    print(grand_array)
 del grand_array[0]
 ##print(grand_array)
+
 count2 = 0
-for i in grand_array:
-    numero = round(float(i[9])/float(i[14]), 2)
-    number_array.append(numero)
+##for i in grand_array:
+##    numero = round(float(i[9])/float(i[14]), 2)
+##    number_array.append(numero)
+    
 ##    print(numero)
 ##print(number_array)
 ##print(max(number_array))
-print(grand_array[number_array.index(max(number_array))][0], '@', max(number_array))
+##print(grand_array[number_array.index(max(number_array))][0], '@', max(number_array))
